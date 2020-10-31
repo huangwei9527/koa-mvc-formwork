ef0de0443c9988552600543c58b4c99e8d112c6d
huangwei <1240413812@qq.com>
2020-11-01 00:44:01 +0800

    去掉console

--- controller/user.js
+++ controller/user.js
@@ -7,7 +7,6 @@ module.exports = app => ({
 	async sellHelloEjs(ctx) {
 		let {$service} = app;
 		let userData = await $service.user.getUser();
-		// userData = userData.toObject();
 		await ctx.render('hello', {message: `我们这里有${userData.length}人`})
 	}
 })
